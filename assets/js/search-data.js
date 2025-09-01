6d9adc546c93fa6596fed850509fc4c6e07e03b7
marcelosogoncalves <81628104+marcelosogoncalves@users.noreply.github.com>
2025-09-01 13:34:53 +0000

    Deploying to gh-pages from @ marcelosogoncalves/marcelosogoncalves.github.io@98faef8d2eccb3e48345a5dfc89c5a34edf5f886 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,15 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2.html";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+        },{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{
